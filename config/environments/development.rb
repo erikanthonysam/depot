00af0c086b9630310731aa2209c692b4efa5af4f
Erik Sam <erikanthonysam@gmail.com>
2011-05-21 13:28:45 -0700

    Finished Ch13

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,18 @@ Depot::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+  
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address          => "smtp.gmail.com",
+    :port             => 587,
+    :domain           => "domain.of.sender.net",
+    :authentication   => "plain",
+    :user_name        => "dave",
+    :password         => "secret",
+    :enable_starttls_auto   => true
+  }
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
